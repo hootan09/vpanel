955eccefbc4060b21cd0daab094da74d40475a98
mhdtjk <mehrdad.tjk@gmail.com>
2023-01-09 13:30:19 +0330

    ssr redirect

--- app/page.js
+++ app/page.js
@@ -1,15 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation';
 
-//Redirect to Login Page
-
-import {useRouter, usePathname, useSearchParams} from 'next/navigation';
-import { useEffect } from 'react';
-
-export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/signin");
-  }, [])
-  
-  return (<></>);
+export default function page(){
+    redirect('/signin')
 }
